9f690881fdd637757964b126c049f6419f8075cc
Marcos <marcosgabriel79355@gmail.com>
2023-11-28 15:36:31 -0300

    Erro

--- src/main/java/br/com/gm2/dev/Demo1Application.java
+++ src/main/java/br/com/gm2/dev/Demo1Application.java
@@ -2,14 +2,15 @@ package br.com.gm2.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Demo1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {SpringApplication.run(Demo1Application.class, args);}
 
-		SpringApplication.run(Demo1Application.class, args);
-
+	@GetMapping("inicio")
+	public String inicio() {
+		return "ola Spring";
 	}
-
 }
