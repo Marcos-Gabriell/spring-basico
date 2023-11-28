2fbf7be9e927a21735f1eb2a701b42c9d98d59d0
Marcos <marcosgabriel79355@gmail.com>
2023-11-28 10:10:00 -0300

    Initial commit

---
+++ src/main/java/br/com/gm2/dev/Demo1Application.java
@@ -0,0 +1,15 @@
+package br.com.gm2.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+
+	}
+
+}
