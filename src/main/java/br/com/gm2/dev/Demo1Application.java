d05bab593a679f3fc1da3c30c5188afa313a2557
Marcos <marcosgabriel79355@gmail.com>
2023-11-28 15:37:00 -0300

    Correção

--- src/main/java/br/com/gm2/dev/Demo1Application.java
+++ src/main/java/br/com/gm2/dev/Demo1Application.java
@@ -3,13 +3,15 @@ package br.com.gm2.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo1Application {
 
 	public static void main(String[] args) {SpringApplication.run(Demo1Application.class, args);}
 
-	@GetMapping("inicio")
+	@GetMapping("/inicio")
 	public String inicio() {
 		return "ola Spring";
 	}
